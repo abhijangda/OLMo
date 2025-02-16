54fba7ed954cbfcfa5902d4df216f00f57c85a62
Abhinav Jangda <abhijangda@gmail.com>
2025-02-15 23:27:05 -0800

    u

--- train_on_vc.py
+++ train_on_vc.py
@@ -12,32 +12,36 @@ def run(c):
     if s != 0:
         print("Error ", o)
 
+SSH = "ssh -F /ddn/ajangda/ssh-config"
+SCP = "scp -F /ddn/ajangda/ssh-config"
+
 if nodes > 1:
-    run("rm -rf /home/aiscuser/ajangda/OLMo/step*")
-    run("rm -rf /home/aiscuser/ajangda/OLMo/train_data/")
-    run("cd /home/aiscuser/ajangda/ ; rm -rf OLMo.zip ; zip -r OLMo.zip /home/aiscuser/ajangda/OLMo/")
+    run("rm -rf ~/ajangda/OLMo/step*")
+    run("rm -rf ~/ajangda/OLMo/train_data/")
+    run("rm -rf ~/ajangda/OLMo/latest/")
+    run("cd ~/ajangda/ ; rm -rf OLMo.zip ; zip -r OLMo.zip ~/ajangda/OLMo/")
     commands = ""
     for node in range(nodes):
         if node != 0:
-            commands += f"ssh node-{node} 'rm -rf /home/aiscuser/ajangda ; mkdir /home/aiscuser/ajangda' \n"
+            commands += f"{SSH} node-{node} 'rm -rf ~/ajangda ; mkdir ~/ajangda' \n"
     
     for node in range(nodes):
         if node != 0:
-            commands += f"scp /home/aiscuser/ajangda/OLMo.zip node-{node}:/home/aiscuser/ajangda/OLMo.zip &\n"
+            commands += f"{SCP} ~/ajangda/OLMo.zip node-{node}:~/ajangda/OLMo.zip &\n"
     commands += "wait\n"
     for node in range(nodes):
         if node != 0:
-            commands += f"ssh node-{node} 'unzip -o /home/aiscuser/ajangda/OLMo.zip -d /' &\n"
+            commands += f"{SSH} node-{node} 'unzip -o ~/ajangda/OLMo.zip -d /' &\n"
     commands += "wait\n"
     for node in range(nodes):
-        commands += f"ssh node-{node} 'cd /home/aiscuser/ajangda/OLMo/ ; pip install -e .[all] ; pip install ./pyfastkron-1.0.1-py3-none-any.whl; pip install aioshutil' &\n"
+        commands += f"{SSH} node-{node} 'cd ~/ajangda/OLMo/ ; pip install -e .[all] ; pip install ./pyfastkron-1.0.1-py3-none-any.whl; pip install aioshutil' &\n"
     
     run(commands)
     
         # run(f"ssh node-{node} killall -SIGKILL /home/ajangda/anaconda3/envs/mscclpp/bin/python")
 #sys.exit(0)
 #for node in range(nodes):
-#    run(f"ssh node-{node} 'cd /home/aiscuser/ajangda/OLMo/ ; pip install -e .[all] ; pip install ./pyfastkron-1.0.1-py3-none-any.whl; pip install aioshutil'")
+#    run(f"ssh node-{node} 'cd ~/ajangda/OLMo/ ; pip install -e .[all] ; pip install ./pyfastkron-1.0.1-py3-none-any.whl; pip install aioshutil'")
 
 export_envs = "export NCCL_PROTO=Simple ; export NCCL_ALGO=Ring ; export TORCH_INDUCTOR_BACKEND=cpp ;" #"export NCCL_P2P_DISABLE=WARN ; export TORCH_DISTRIBUTED_DEBUG=DETAIL ; export TORCH_CPP_LOG_LEVEL=INFO; "
 step = sys.argv[4]
@@ -46,14 +50,14 @@ import random
 rdzv_id = random.randint(1000, 9999)
 port = random.randint(10000,65536)
 torchrun = f"torchrun --rdzv_id={rdzv_id} --rdzv_backend=c10d --rdzv_endpoint={ip}:{port} --nnodes {nodes} --nproc_per_node=8 scripts/train.py configs/official-1124/OLMo2-{train_id}.yaml --save_overwrite --wandb.name={train_id} --fsdp.wrapping_strategy=null --save_interval_ephemeral=100"
-load_path_node_1 = f"/home/aiscuser/mnt-node-0/ajangda/step{step}/"
-load_path_node_0 = f"/home/aiscuser/ajangda/step{step}/"
+load_path_node_1 = f"~/mnt-node-0/ajangda/step{step}/"
+load_path_node_0 = f"~/ajangda/step{step}/"
 for node in range(nodes):
     olmo_data = f"/scratch/whitneyblobstore/OLMo-data/{train_id}"
     remote_folder=f"{olmo_data}/node-{node}/"
     os.makedirs(remote_folder, exist_ok=True)
-    stdout=("stdout" if node==0 else "/home/aiscuser/mnt-node-0/ajangda/OLMo/stdout")+ str(node) #f"{remote_folder}/stdout"
+    stdout=("stdout" if node==0 else "~/mnt-node-0/ajangda/OLMo/stdout")+ str(node) #f"{remote_folder}/stdout"
     load_path = load_path_node_0 if node == 0 else load_path_node_1
     load_path = f"--load_path={load_path}"
-    nohup_wrap = f"cd /home/aiscuser/ajangda/OLMo ; nohup {torchrun}  &>> {stdout} &"
-    run(f"ssh node-{node} '{export_envs} {nohup_wrap}'")
+    nohup_wrap = f"cd ~/ajangda/OLMo ; nohup {torchrun}  &>> {stdout} &"
+    run(f"{SSH} node-{node} '{export_envs} {nohup_wrap}'")
